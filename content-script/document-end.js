cbbf2331cd69c1e4a794fbee86907ca6a61e010b
Monyer <monyer@126.com>
2020-09-20 13:12:13 +0800

    bug fix

--- content-script/document-end.js
+++ content-script/document-end.js
@@ -3,41 +3,43 @@
  * 判断fingerprintjs2的插件。
  */
 var injectEnd = function() {
-  //某蜜罐用了两个全局变量：token、path。token为使用短横线链接的随机值，path为js_开头的目录
-  if (window.token !== undefined && window.path !== undefined) {
-    if (typeof token == "string" && token.includes("-") &&
-      typeof path == "string" && path.includes("js_")) {
-      document.documentElement.innerHTML = "This is a Honeypot.";
+  window.addEventListener("load", function() {
+    //某蜜罐用了两个全局变量：token、path。token为使用短横线链接的随机值，path为js_开头的目录
+    if (window.token !== undefined && window.path !== undefined) {
+      if (typeof token == "string" && token.includes("-") &&
+        typeof path == "string" && path.includes("js_")) {
+        document.documentElement.innerHTML = "This is a Honeypot.";
+        window.top.postMessage({
+          msgType: "honeypotAlert",
+          msgData: {
+            blockInfo: "token=" + token + " | path=" + path
+          }
+        }, '*');
+      }
+    }
+    //如果使用了fingerprint插件，全局函数的内部存在x64hash128和getV18两个对外公开的函数，以此作为判断。
+    var fpDetect = Object.keys(window).filter(func => func != 'webkitStorageInfo' && typeof window[func] == "function" && window[func]['x64hash128'] && window[func]['getV18']);
+
+    if (fpDetect == 0) {
+      //fingerprintjs pro
+      fpDetect = (typeof window.FP == "object" || typeof window.fpLayer !== "undefined") ? ["fpPro"] : [];
+    }
+
+    if (fpDetect.length !== 0) {
       window.top.postMessage({
-        msgType: "honeypotAlert",
+        msgType: "fingerprint2",
         msgData: {
-          blockInfo: "token=" + token + " | path=" + path
+          fp: fpDetect.join(',')
         }
       }, '*');
     }
-  }
-  //如果使用了fingerprint插件，全局函数的内部存在x64hash128和getV18两个对外公开的函数，以此作为判断。
-  var fpDetect = Object.keys(window).filter(func => func != 'webkitStorageInfo' && typeof window[func] == "function" && window[func]['x64hash128'] && window[func]['getV18']);
-
-  if (fpDetect == 0) {
-    //fingerprintjs pro
-    fpDetect = (typeof window.FP == "object" || typeof window.fpLayer !== "undefined") ? ["fpPro"] : [];
-  }
-
-  if (fpDetect.length !== 0) {
-    window.top.postMessage({
-      msgType: "fingerprint2",
-      msgData: {
-        fp: fpDetect.join(',')
-      }
-    }, '*');
-  }
-
+  });
   document.documentElement.dataset.csescriptallow = true;
 };
 
 var scriptEnd_1 = document.createElement('script');
-scriptEnd_1.textContent = "document.addEventListener('load'," + injectEnd + ");";
+// scriptEnd_1.textContent = "document.addEventListener('load'," + injectEnd + ");";
+scriptEnd_1.textContent = "(" + injectEnd + ")()";
 document.documentElement.appendChild(scriptEnd_1);
 
 if (!document.documentElement.dataset.csescriptallow) {
