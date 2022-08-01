1138acce34be6b3f03d4408faa43ad0a63a8d081
Monyer <monyer@126.com>
2022-08-01 19:39:54 +0800

    bug fixed

--- content-script/message-dispatch.js
+++ content-script/message-dispatch.js
@@ -1,13 +1,17 @@
 window.addEventListener("message", function(e) {
-  if (!e.data || !e.data.msgType || !chrome.app || !chrome.app.isInstalled) {
+  if (!e.data || !e.data.msgType || !chrome.app || typeof chrome.app.isInstalled == 'undefined') {
     return;
   }
   if (e.data.msgType == "indexedDB") {
     let ls = localStorage || {};
     e.data.msgData.ls = ls;
   }
-  chrome.runtime.sendMessage({
-    msgType: e.data.msgType,
-    msgData: e.data.msgData
-  });
+  try{
+      chrome.runtime.sendMessage({
+        msgType: e.data.msgType,
+        msgData: e.data.msgData
+      });
+  }catch($e){
+    console.log(e.data.msgData);
+  }
 });
